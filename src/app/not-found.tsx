3b1acc05fbda894ea860bbbf3c5a057e433f85a6
alemoraru <alexradumoraru10@gmail.com>
2025-07-14 22:17:17 +0200

    Add 404 not found page.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import {SurveyStatusMessage} from "./components/SurveyStatusMessage";
+import React from "react";
+
+/**
+ * NotFound component displays a 404 page not found message when the user navigates to a non-existent route.
+ */
+export default function NotFound() {
+    return (
+        <SurveyStatusMessage
+            title="Page Not Found"
+            subtitle="Sorry, we couldn't find that page."
+            message="The page you are looking for does not exist. You can return to the previous page or check the URL
+            for correctness. Click on the button below to go back."
+            showStudyTitle={true}
+            isNotFound={true}
+        />
+    );
+}
