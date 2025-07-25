7d57ee0e04e6efa39643f442ae83447246474bd7
alemoraru <alexradumoraru10@gmail.com>
2025-07-25 23:06:13 +0200

    Movel panels to separate module

--- src/app/components/panels/Part2Step4Panel.tsx
+++ src/app/components/panels/Part2Step4Panel.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {CodeEditor} from "./editor/CodeEditor";
-import {LoaderToast} from "./toast/LoaderToast";
-import {ErrorToast} from "./toast/ErrorToast";
-import {ConfirmChoiceModal, ConfirmChoiceModalType} from "./toast/ConfirmChoiceModal";
+import {CodeEditor} from "../editor/CodeEditor";
+import {LoaderToast} from "../toast/LoaderToast";
+import {ErrorToast} from "../toast/ErrorToast";
+import {ConfirmChoiceModal, ConfirmChoiceModalType} from "../toast/ConfirmChoiceModal";
 
 interface PanelProps {
     code: string;
